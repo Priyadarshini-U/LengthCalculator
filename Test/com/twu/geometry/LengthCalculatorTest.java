491c1e6ebbf0b70a737f51de933ebb1294e5102d
priyadarshini <priyadr@thoughtworks.com>
2015-08-25 16:18:17 +0530

    ant build

--- Test/com/twu/geometry/LengthCalculatorTest.java
+++ Test/com/twu/geometry/LengthCalculatorTest.java
@@ -4,22 +4,20 @@ import com.twu.geometry.Coordinate;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Created by admin on 25/08/15.
- */
+
 public class LengthCalculatorTest {
 
     @Test
-    public void testcase1(){
-        Double actual= LengthCalculator.getLineLength(new Coordinate(1, 1), new Coordinate(1, 1));
-        Double expected=new Double(0);
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void testcase2(){
-        Double actual= LengthCalculator.getLineLength(new Coordinate(0, 0), new Coordinate(1, 1));
-        Double expected=new Double(Math.sqrt(2));
-        Assert.assertEquals(expected,actual);
+    public void testcase1() {
+        Double actual = LengthCalculator.getLineLength(new Coordinate(1, 1), new Coordinate(1, 1));
+        Double expected = new Double(0);
+        Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void testcase2() {
+        Double actual = LengthCalculator.getLineLength(new Coordinate(0, 0), new Coordinate(1, 1));
+        Double expected = new Double(Math.sqrt(2));
+        Assert.assertEquals(expected, actual);
+    }
 }
