7a4d12013dcef3c8e3af088863d760305a13af46
priyadarshini <priyadr@thoughtworks.com>
2015-08-26 08:47:22 +0530

    Case1-same point

--- src/com/twu/geometry/LengthCalculator.java
+++ src/com/twu/geometry/LengthCalculator.java
@@ -1,8 +1,7 @@
 package com.twu.geometry;
 public class LengthCalculator {
 
-    public static double getLineLength(Coordinate coordinate, Coordinate coordinate1) {
-        Double length=Math.sqrt(Math.pow(coordinate.getX()-coordinate1.getX(),2.0)+Math.pow(coordinate.getY() - coordinate1.getY(), 2.0));
-        return length;
+    public static int getLineLength(Coordinate coordinate, Coordinate coordinate1) {
+        return 0;
     }
 }
